c643fffead19faa81db462425afb11956a4cce1b
Prashant Zanzan <139844406+prashantzanzan25@users.noreply.github.com>
2023-11-17 15:48:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 function shortcut(s1, s2) {
   // your code here
+	if (s1.length === 0 || s2.length === 0) {
+    return '';
+  }
+
+  // Return the initial letters of the input strings
+  return s1[0].toUpperCase() + s2[0].toLowerCase();
 }
 
 // Do not change the code below.
 const s1 = prompt("Enter s1:");
 const s2 = prompt("Enter s2:");
 alert(shortcut(s1, s2));
+
+
