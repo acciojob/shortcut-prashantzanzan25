bc2e9a18424351dc7f29e538420ee13192eb8736
Prashant Zanzan <139844406+prashantzanzan25@users.noreply.github.com>
2024-04-10 12:09:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,10 @@
 function shortcut(s1, s2) {
   // your code here
-	if (s1.length === 0 || s2.length === 0) {
-    return '';
-  }
-	
-
-  // Return the initial letters of the input strings
-  return s1[0].toUpperCase() + s2[0].toLowerCase();
+	const initial1 = s1.charAt(0);
+	const initial2 = s2.charAt(0);
+  
+	return initial1 + initial2;
 }
-
 // Do not change the code below.
 const s1 = prompt("Enter s1:");
 const s2 = prompt("Enter s2:");
